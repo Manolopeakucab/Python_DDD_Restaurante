73e19d0667c8b5d48ab3a4f8376b19e4b2e0a261
Manolopeakucab <mapico.20@est.ucab.edu.ve>
2024-07-03 11:29:27 -0400

    medida ingrediente created

---
+++ Include/API/Ingrediente/Domain/ValueObjects/Unidad_Medida_Ingrediente.py
@@ -0,0 +1,23 @@
+from Include.API.COMMON.Domain.valueObjects.Value_object import ValueObject
+
+class Unidad_medida_ingrediente_VO(ValueObject):
+    medida: int
+
+    def __init__(self, medida:int):
+        medida = Unidad_medida_ingrediente_VO.valid(medida);
+        if medida is None:
+            raise ValueError("La medidas del ingrediente no puede ser vacio")
+        self._medida = medida
+
+    def equals(self, other: 'Unidad_medida_ingrediente_VO') -> bool:
+        return self._medida== other._medida
+    
+    def valid(self) -> bool:
+        return len(self.medida) > 0
+    
+    def get_value(self) -> str:
+        return self
+    
+    staticmethod
+    def create(nombre: str) -> 'Unidad_medida_ingrediente_VO':
+        return Unidad_medida_ingrediente_VO(nombre)
